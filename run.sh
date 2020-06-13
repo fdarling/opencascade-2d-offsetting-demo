039880563a6e0dda0ac4f38f217fa8aa0bf0d0c4
Forest Darling <fdarling@gmail.com>
2020-06-12 22:53:49 -0500

    rewrote run.sh to more easily/cleanly switch test cases

--- run.sh
+++ run.sh
@@ -1,13 +1,14 @@
-make
-#./opencascade_demo offset ./tests/input_001.txt ./tests/output_simple_offset.txt 0.2 arc
-#./opencascade_demo booleans  ./tests/input_001.txt ./tests/output_hard.txt ./tests/booleans_output_001.txt difference_reversed
-#./opencascade_demo booleans  ./tests/polygon_without_2_holes.txt ./tests/input_circle_offset.txt ./tests/booleans_output_001.txt union
+#!/bin/sh
+#CPP_ARGS="offset ./tests/input_001.txt ./tests/output_simple_offset.txt 0.2 arc"
+#CPP_ARGS="booleans ./tests/input_001.txt ./tests/output_hard.txt ./tests/booleans_output_001.txt difference_reversed"
+#CPP_ARGS="booleans ./tests/polygon_without_2_holes.txt ./tests/input_circle_offset.txt ./tests/booleans_output_001.txt union"
+#CPP_ARGS="booleans ./tests/input_001.txt ./tests/input_001_small_off_10.txt ./tests/booleans_output_001.txt difference"
+#CPP_ARGS="offset ./tests/polygon_with_2_holes.txt ./tests/polygon_with_2_holes_offset.txt 0.2 arc"
+#CPP_ARGS="offset ./tests/input_circle_3_parts.txt ./tests/input_circle_3_parts_offset.txt 0.2 arc"
+CPP_ARGS="offset ./tests/input_hard_offset.txt ./tests/output_hard_offset.txt -0.2 arc"
 
-#./opencascade_demo booleans  ./tests/input_001.txt ./tests/input_001_small_off_10.txt ./tests/booleans_output_001.txt difference
+VIS_ARGS=`echo ${CPP_ARGS} | awk '/\.txt/' RS=" " ORS=" "`
 
-#./opencascade_demo offset ./tests/polygon_with_2_holes.txt ./tests/polygon_with_2_holes_offset.txt 0.2 arc
-#./opencascade_demo offset ./tests/input_circle_3_parts.txt ./tests/input_circle_3_parts_offset.txt 0.2 arc
-
-./opencascade_demo offset ./tests/input_hard_offset.txt ./tests/output_hard_offset.txt -0.2 arc
-#python3 vis.py ./tests/polygon_without_2_holes.txt ./tests/input_circle_offset.txt ./tests/booleans_output_001.txt
-python3 vis.py ./tests/input_hard_offset.txt  ./tests/output_hard_offset.txt &
+make && \
+./opencascade_demo $CPP_ARGS && \
+python3 vis.py $VIS_ARGS
